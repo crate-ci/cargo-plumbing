b7d9e8ca7e7ab9b4d2c865d7a36d1866ed3eedce
secona <secona00@gmail.com>
2025-06-24 00:12:56 +0700

    feat(locate-manifest): Simple implementation
    feat: Write JSON string to out

--- examples/check.rs
+++ examples/check.rs
@@ -1,11 +1,28 @@
+use std::process::Command;
+
 use cargo_plumbing::CargoResult;
+use cargo_plumbing_schemas::locate_manifest::ManifestLocation;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
 struct Args {}
 
 fn run(_args: &Args) -> CargoResult<()> {
-    anyhow::bail!("not implemented!");
+    let locate_manifest: ManifestLocation = {
+        let output = Command::new("cargo")
+            .args(["run", "plumbing", "locate-manifest"])
+            .output()
+            .expect("failed to run locate-project");
+
+        let stdout = String::from_utf8(output.stdout)?;
+
+        serde_json::from_str(&stdout)
+            .expect("failed to parse JSON from locate-project")
+    };
+
+    let manifest_path = locate_manifest.manifest_path;
+
+    anyhow::bail!("check for {manifest_path} is not implemented!");
 }
 
 fn main() {
